c33ea0dfb6851f10e394e6125ed13eabf911800a
kg8m <takumi.kagiyama@gmail.com>
2023-01-15 15:08:07 +0900

    Log "Executed: ..." message only when some configs have been changed

--- denops/detect-indent/options.ts
+++ denops/detect-indent/options.ts
@@ -1,36 +1,45 @@
 import type { Denops } from "https://deno.land/x/denops_std@v3.9.1/mod.ts";
 import * as vimOptions from "https://deno.land/x/denops_std@v3.9.1/option/mod.ts";
-import { batch } from "https://deno.land/x/denops_std@v3.9.1/batch/mod.ts";
 import * as logger from "./logger.ts";
 import { isEmptyObject } from "./util.ts";
 
-export type Options = {
-  expandtab: false;
-} | {
-  expandtab: true;
-  shiftwidth: Awaited<ReturnType<typeof vimOptions.shiftwidth["get"]>>;
-} | Record<never, never>;
+export type Options =
+  | { expandtab: false }
+  | {
+    expandtab: true;
+    shiftwidth: Awaited<ReturnType<typeof vimOptions.shiftwidth["get"]>>;
+  }
+  | Record<never, never>;
 
 export async function set(denops: Denops, options: Options): Promise<void> {
   if (isEmptyObject(options)) {
     return;
   }
 
-  const executionMessage = ["Executed:", "setlocal"];
+  let reallyChanged = false;
+  const changes = [];
 
-  await batch(denops, async (denops) => {
-    if ("expandtab" in options && options.expandtab != null) {
+  if ("expandtab" in options && options.expandtab != null) {
+    changes.push(`${options.expandtab ? "" : "no"}expandtab`);
+    const currentExpandtab = await vimOptions.expandtab.getLocal(denops);
+
+    if (!!options.expandtab !== !!currentExpandtab) {
+      reallyChanged = true;
       await vimOptions.expandtab.setLocal(denops, options.expandtab);
-      executionMessage.push(
-        `${options.expandtab ? "" : "no"}expandtab`,
-      );
     }
+  }
 
-    if ("shiftwidth" in options && options.shiftwidth != null) {
+  if ("shiftwidth" in options && options.shiftwidth != null) {
+    changes.push(`shiftwidth=${options.shiftwidth}`);
+    const currentShiftwidth = await vimOptions.shiftwidth.getLocal(denops);
+
+    if (options.shiftwidth !== currentShiftwidth) {
+      reallyChanged = true;
       await vimOptions.shiftwidth.setLocal(denops, options.shiftwidth);
-      executionMessage.push(`shiftwidth=${options.shiftwidth}`);
     }
-  });
+  }
 
-  await logger.info(denops, ...executionMessage);
+  if (reallyChanged) {
+    await logger.info(denops, ...["Executed:", "setlocal", ...changes]);
+  }
 }
